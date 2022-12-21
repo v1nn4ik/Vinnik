ec5699f4e8929196c3612b83488efa8200ab3cf0
v1nn4ik <vinnik-03@mail.ru>
2022-12-21 22:46:27 +0500

    добавил 3.3.1

--- 3_3/3_3_1.py
+++ 3_3/3_3_1.py
@@ -16,19 +16,19 @@ df_api_currency = pd.DataFrame(columns=["date", "BYR", "USD", "EUR", "KZT", "UAH
 for i in range(0, len(months)):
     url = f'https://www.cbr.ru/scripts/XML_daily.asp?date_req=01/{months[i]}d=1'
     response = requests.get(url)
-    currencies = pd.read_xml(response.text)
-    currencies2 = currencies.loc[currencies['CharCode'].isin(['BYN', 'BYR', 'EUR', 'KZT', 'UAH', 'USD'])]
+    curr = pd.read_xml(response.text)
+    filtered_curr = curr.loc[curr['CharCode'].isin(['BYN', 'BYR', 'EUR', 'KZT', 'UAH', 'USD'])]
 
-    BYR = float(currencies2.loc[currencies2['CharCode'].isin(['BYR', 'BYN'])]['Value'].values[0].replace(',', '.')) / \
-          (currencies2.loc[currencies2['CharCode'].isin(['BYR', 'BYN'])]['Nominal'].values[0])
-    EUR = float(currencies2.loc[currencies2['CharCode'] == 'EUR']['Value'].values[0].replace(',', '.')) / \
-          (currencies2.loc[currencies2['CharCode'] == 'EUR']['Nominal'].values[0])
-    KZT = float(currencies2.loc[currencies2['CharCode'] == 'KZT']['Value'].values[0].replace(',', '.')) / \
-          (currencies2.loc[currencies2['CharCode'] == 'KZT']['Nominal'].values[0])
-    UAH = float(currencies2.loc[currencies2['CharCode'] == 'UAH']['Value'].values[0].replace(',', '.')) / \
-          (currencies2.loc[currencies2['CharCode'] == 'UAH']['Nominal'].values[0])
-    USD = float(currencies2.loc[currencies2['CharCode'] == 'USD']['Value'].values[0].replace(',', '.')) / \
-          (currencies2.loc[currencies2['CharCode'] == 'USD']['Nominal'].values[0])
+    BYR = float(filtered_curr.loc[filtered_curr['CharCode'].isin(['BYR', 'BYN'])]['Value'].values[0].replace(',', '.')) / \
+          (filtered_curr.loc[filtered_curr['CharCode'].isin(['BYR', 'BYN'])]['Nominal'].values[0])
+    EUR = float(filtered_curr.loc[filtered_curr['CharCode'] == 'EUR']['Value'].values[0].replace(',', '.')) / \
+          (filtered_curr.loc[filtered_curr['CharCode'] == 'EUR']['Nominal'].values[0])
+    KZT = float(filtered_curr.loc[filtered_curr['CharCode'] == 'KZT']['Value'].values[0].replace(',', '.')) / \
+          (filtered_curr.loc[filtered_curr['CharCode'] == 'KZT']['Nominal'].values[0])
+    UAH = float(filtered_curr.loc[filtered_curr['CharCode'] == 'UAH']['Value'].values[0].replace(',', '.')) / \
+          (filtered_curr.loc[filtered_curr['CharCode'] == 'UAH']['Nominal'].values[0])
+    USD = float(filtered_curr.loc[filtered_curr['CharCode'] == 'USD']['Value'].values[0].replace(',', '.')) / \
+          (filtered_curr.loc[filtered_curr['CharCode'] == 'USD']['Nominal'].values[0])
 
     df_api_currency.loc[i] = [f'{months[i][3:]}-{months[i][:2]}', BYR, EUR, KZT, UAH, USD]
 
