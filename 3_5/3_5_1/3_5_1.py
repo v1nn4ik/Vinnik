a07393c18a89b6a0330466b71a18d39b58b88ef6
Konstantin <v1nn4ik@icloud.com>
2022-12-29 21:43:20 +0500

    добавил 3_5_1

---
+++ 3_5/3_5_1/3_5_1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sqlite3
+
+con = sqlite3.connect('/3_5/3_5_1/curr.sqlite')
+c = con.cursor()
+
+df = pd.read_csv('/3_5/3_5_1/curr.csv')
+df = df.to_sql('currencies', con, if_exists='replace', index=False)
+con.commit()
